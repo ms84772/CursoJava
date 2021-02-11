85c65ab617f6a2556b9a28f32fc6aa94e74e019f
jlopezyo <jlopezyo@hotmail.com>
2021-02-10 20:21:26 -0800

    new classes added

---
+++ src/james/inner/EmployeeController.java
@@ -0,0 +1,34 @@
+package james.inner;
+
+import java.util.Date;
+
+
+public class EmployeeController {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		Employee emp = new Employee();
+		emp.setFirstName("Mauricio");
+		emp.setLastName("Sahagun");
+		emp.setDob(new Date());
+		
+		Employee.Address address = emp.new Address();
+		
+		address.setCity("Plano");
+		address.setState("TX");
+		address.setStreet1("1432 Debond Dr");
+		address.setZipCode("75075");
+		
+		
+		Department department = new Department();
+		
+		
+		
+		
+
+	}
+
+}
